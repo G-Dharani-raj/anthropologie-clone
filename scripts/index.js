0a8c9dd9b9b1a43f2a00a84446210cf8eda0f7a7
momin-mohammed-imran <mominmohammedimran02@gmail.com>
2022-11-09 22:23:29 +0530

    day-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 // Code here
+    document.querySelector("#signinimg").addEventListener("click",code)
+    document.querySelector("#signin").addEventListener("click",code)
+    
+    function code(){
+        let signin=document.getElementById("signin_signup_form")
+        let signup=document.getElementById("signuppage");
+        let container=document.getElementById("container")
+        signin.style.visibility="visible"
+        signin.style.display="block"
+        container.style.position="fixed"
+        container.style.filter="blur(2px)"
+        document.querySelector("mobilenumber").addEventListener("click",numbersignin)
+        function numbersignin(){
+            signin.style.visibility="visible"
+            signin.style.display="block"
+            signup.style.visibility="visible"
+            signup.style.display="block"
+
+        }
+        document.querySelector("createanaccount").addEventListener("click",signup)
+        function signup(){
+            signup.style.visibility="visible"
+            signup.style.display="block"
+            signin.style.visibility="hidden"
+            signin.style.display="none"
+        }
+
+    }
